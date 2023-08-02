71a4e037945a9082a98c78dc694ed4277ec5cfed
Dallas <71103219+LeleDallas@users.noreply.github.com>
2023-08-02 11:38:55 +0200

    feat(utils): global variables

---
+++ src/utils.tsx
@@ -0,0 +1,58 @@
+import { IconNames } from "./components/iconfont"
+
+export const fullName = "Emanuele Dall'Ara"
+export const email = "emanuele.dallara99@gmail.com"
+export const role = "Software Engineer"
+export const phone = "+39 340 098 1328"
+export const birthday = "Oct 4, 1999"
+export const location = "Cesena, Emilia-Romagna, ITA"
+
+export const navbarItems = ["About", "Resume", "Portfolio", "Blog", "Contact"]
+
+export const serviceItems = [
+    {
+        image: "./src/assets/icon-design.svg",
+        title: "Web design",
+        text: "The most modern and high-quality design made at a professional level."
+    },
+    {
+        image: "./src/assets/icon-dev.svg",
+        title: "Software development",
+        text: "The most modern and high-quality design made at a professional level."
+    },
+    {
+        image: "./src/assets/icon-design.svg",
+        title: "Web design",
+        text: "The most modern and high-quality design made at a professional level."
+    },
+    {
+        image: "./src/assets/icon-design.svg",
+        title: "Web design",
+        text: "The most modern and high-quality design made at a professional level."
+    },
+]
+
+export const technologiesItems = [
+    {
+        image: "./src/assets/icon-design.svg",
+        name: "React",
+        text: "The most modern and high-quality design made at a professional level."
+    },
+    {
+        image: "https://vitejs.dev/logo-with-shadow.png",
+        name: "Vite",
+        text: "The most modern and high-quality design made at a professional level."
+    },
+    {
+        image: "https://vitest.dev/logo.svg",
+        name: "Vitest",
+        text: "The most modern and high-quality design made at a professional level."
+    },
+    {
+        image: "./src/assets/icon-design.svg",
+        name: "Java",
+        text: "The most modern and high-quality design made at a professional level."
+    },
+]
+
+export const socialListIcons: Array<IconNames> = ["linkedin-fill", "github", "leetcode", "stackoverflow"]
