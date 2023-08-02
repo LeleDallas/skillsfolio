78b93153cf44d8a827ddba08cfd399d72f1fa0df
Dallas <71103219+LeleDallas@users.noreply.github.com>
2023-08-02 17:20:47 +0200

    feat: utils

--- src/utils.tsx
+++ src/utils.tsx
@@ -1,4 +1,4 @@
-import { IconNames } from "./components/iconfont"
+import { DataModel, SocialObject } from "./types"
 
 export const fullName = "Emanuele Dall'Ara"
 export const email = "emanuele.dallara99@gmail.com"
@@ -6,6 +6,10 @@ export const role = "Software Engineer"
 export const phone = "+39 340 098 1328"
 export const birthday = "Oct 4, 1999"
 export const location = "Cesena, Emilia-Romagna, ITA"
+export const description = "I'm Creative Director and UI/UX Designer from Sydney, Australia, working in web development and print media. I enjoy turning complex problems into simple, beautiful and intuitive designs."
+export const descriptionDetails = "My job is to build your website so that it is functional and user-friendly but at the same time attractive. Moreover, I add personal touch to your product and make sure that is eye-catching and easy to use. My aim is to bring across your message and identity in the most creative way. I created web design for many famous brand companies."
+
+
 
 export const navbarItems = ["About", "Resume", "Portfolio", "Blog", "Contact"]
 
@@ -21,38 +25,71 @@ export const serviceItems = [
         text: "The most modern and high-quality design made at a professional level."
     },
     {
-        image: "./src/assets/icon-design.svg",
-        title: "Web design",
+        image: "./src/assets/icon-dev.svg",
+        title: "Software development",
         text: "The most modern and high-quality design made at a professional level."
     },
     {
-        image: "./src/assets/icon-design.svg",
-        title: "Web design",
+        image: "./src/assets/icon-dev.svg",
+        title: "Software development",
         text: "The most modern and high-quality design made at a professional level."
     },
 ]
 
-export const technologiesItems = [
+export const technologiesItems: Array<DataModel> = [
     {
-        image: "./src/assets/icon-design.svg",
-        name: "React",
-        text: "The most modern and high-quality design made at a professional level."
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1150px-React-icon.svg.png",
+        title: "React",
+        description: "The most modern and high-quality design made at a professional level.",
+        purpose: "---"
     },
     {
         image: "https://vitejs.dev/logo-with-shadow.png",
-        name: "Vite",
-        text: "The most modern and high-quality design made at a professional level."
+        title: "Vite",
+        description: "The most modern and high-quality design made at a professional level.",
+        purpose: "---"
     },
     {
         image: "https://vitest.dev/logo.svg",
-        name: "Vitest",
-        text: "The most modern and high-quality design made at a professional level."
+        title: "Vitest",
+        description: "The most modern and high-quality design made at a professional level.",
+        purpose: "---"
     },
     {
-        image: "./src/assets/icon-design.svg",
-        name: "Java",
-        text: "The most modern and high-quality design made at a professional level."
+        image: "https://cdn-icons-png.flaticon.com/512/5968/5968282.png",
+        title: "Java",
+        description: "The most modern and high-quality design made at a professional level.",
+        purpose: "---"
+    },
+    {
+        image: "https://cdn-icons-png.flaticon.com/512/6132/6132220.png",
+        title: "Scala",
+        description: "The most modern and high-quality design made at a professional level.",
+        purpose: "---"
+    },
+    {
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1869px-Python-logo-notext.svg.png",
+        title: "Python",
+        description: "The most modern and high-quality design made at a professional level.",
+        purpose: "---"
     },
 ]
 
-export const socialListIcons: Array<IconNames> = ["linkedin-fill", "github", "leetcode", "stackoverflow"]
+export const socialListIcons: Array<SocialObject> = [
+    {
+        url: "https://www.linkedin.com/in/emanuele-dall-ara-40b3311a7/",
+        icon: "linkedin-fill",
+    },
+    {
+        url: "https://github.com/LeleDallas",
+        icon: "github"
+    },
+    {
+        url: "https://leetcode.com/LeleDallas/",
+        icon: "leetcode"
+    },
+    {
+        url: "https://stackoverflow.com/users/20091055/dallas",
+        icon: "stackoverflow"
+    },
+]
