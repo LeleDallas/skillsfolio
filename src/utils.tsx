439c2be6bbed3e2f57d57ef9a629452858dd359d
Dallas <71103219+LeleDallas@users.noreply.github.com>
2023-08-02 19:22:02 +0200

    refactor: update text

--- src/utils.tsx
+++ src/utils.tsx
@@ -1,4 +1,4 @@
-import { DataModel, SocialObject } from "./types"
+import { BookObject, DataModel, SocialObject } from "./types"
 
 export const fullName = "Emanuele Dall'Ara"
 export const email = "emanuele.dallara99@gmail.com"
@@ -6,17 +6,24 @@ export const role = "Software Engineer"
 export const phone = "+39 340 098 1328"
 export const birthday = "Oct 4, 1999"
 export const location = "Cesena, Emilia-Romagna, ITA"
-export const description = "I'm Creative Director and UI/UX Designer from Sydney, Australia, working in web development and print media. I enjoy turning complex problems into simple, beautiful and intuitive designs."
-export const descriptionDetails = "My job is to build your website so that it is functional and user-friendly but at the same time attractive. Moreover, I add personal touch to your product and make sure that is eye-catching and easy to use. My aim is to bring across your message and identity in the most creative way. I created web design for many famous brand companies."
+export const description = "I'm a Software Engineer from Cesena, Italy, with a passion for creating high-performance web and mobile applications. I enjoy turning complex problems into simple, beautiful and intuitive designs."
+export const descriptionDetails = `I love building high-performance web and mobile apps. From requirements to deployment, I handle the entire development lifecycle with passion and precision. 
+Keeping up with the latest trends in the industry is a priority, ensuring I always deliver top-notch, user-friendly solutions.
+Let's collaborate and create something amazing together!`
 
 
 
-export const navbarItems = ["About", "Resume", "Portfolio", "Blog", "Contact"]
+export const navbarItems = ["About", "Resume", "Portfolio", "Bookshelf", "Contact"]
 
 export const serviceItems = [
     {
         image: "./assets/icon-design.svg",
-        title: "Web design",
+        title: "UI/UX Designer",
+        text: "The most modern and high-quality design made at a professional level."
+    },
+    {
+        image: "./assets/icon-dev.svg",
+        title: "DevOps",
         text: "The most modern and high-quality design made at a professional level."
     },
     {
@@ -26,12 +33,7 @@ export const serviceItems = [
     },
     {
         image: "./assets/icon-dev.svg",
-        title: "Software development",
-        text: "The most modern and high-quality design made at a professional level."
-    },
-    {
-        image: "./assets/icon-dev.svg",
-        title: "Software development",
+        title: "Mobile App Developer",
         text: "The most modern and high-quality design made at a professional level."
     },
 ]
@@ -91,5 +93,76 @@ export const socialListIcons: Array<SocialObject> = [
     {
         url: "https://stackoverflow.com/users/20091055/dallas",
         icon: "stackoverflow"
+    },
+]
+
+
+export const bookList: Array<BookObject> = [
+    {
+        title: "So good they can't ignore you",
+        description: "",
+        category: "Personal Growth",
+        author: "",
+        image: "https://is1-ssl.mzstatic.com/image/thumb/Publication123/v4/b7/08/76/b708761e-45c8-43af-4e18-21095ef191f6/9781455509102.jpg/1200x600wz.png",
+        link: "",
+    },
+    {
+        title: "The seven spiritual laws of success",
+        description: "",
+        category: "Personal Growth",
+        author: "",
+        image: "https://is1-ssl.mzstatic.com/image/thumb/Publication124/v4/94/23/22/942322b7-5156-c4df-d23d-78242b668a8c/52731bbf-38f0-4bb0-a226-9e2f5089a8d4_cover_image.jpg/1200x600wz.png",
+        link: "",
+    },
+    {
+        title: "The energy codes",
+        description: "Personal Growth",
+        category: "",
+        author: "",
+        image: "https://is1-ssl.mzstatic.com/image/thumb/Publication113/v4/f2/26/57/f2265745-d601-d9ee-91ed-f7fe5cbfa8e1/9781501169328.jpg/1200x630wz.png",
+        link: "",
+    },
+    {
+        title: "Hustle Harder, Hustle Smarter",
+        description: "",
+        category: "Personal Growth",
+        author: "",
+        image: "https://is1-ssl.mzstatic.com/image/thumb/Music123/v4/fb/2b/bb/fb2bbb1d-68cf-3350-281f-8e1b402e6032/9780062953834.jpg/1200x600wp.png",
+        link: "",
+    },
+    {
+        title: "Decoded",
+        description: "",
+        category: "Personal Growth",
+        author: "",
+        image: "https://is1-ssl.mzstatic.com/image/thumb/Publication125/v4/f6/46/a9/f646a92a-d9d8-cac6-51b5-3af19babf7a1/9781588369598.jpg/1200x630wz.png",
+        link: "",
+    },
+    {
+        title: "The Marathon Don't Stop",
+        description: `This “beautiful tribute to a legendary artist” (Quincy Jones) is the first in-depth biography of Nipsey Hussle, the hip-hop mogul, artist, and activist whose transformative legacy inspired a generation with his motivational lyrics and visionary business savvy—before he was tragically shot down in the very neighborhood he was dedicated to building up.
+        For Nipsey Hussle, “The Marathon” was more than a mixtape title or the name of a clothing store; it was a way of life, a metaphor for the relentless pursuit of excellence and the willpower required to overcome adversity day after day. Hussle was determined to win the race to success on his own terms, and he wanted to see his whole community in the winner’s circle with him.
+        A moving and powerful exploration of an extraordinary artist, The Marathon Don’t Stop places Hussle in historical context and unpacks his complex legacy. Combining on-the-ground reporting and candid interviews, “Rob Kenner has given us the book the world—and hip-hop and pop culture—has been waiting for…one that should be celebrated alongside the best biographies ever about iconic figures we have loved—and lost” (Kevin Powell, author of When We Free the World).`,
+        category: "Personal Growth",
+        author: "",
+        image: "https://is1-ssl.mzstatic.com/image/thumb/Music114/v4/40/a7/65/40a76590-4667-1182-62cc-b7d199a14087/9781797104935.jpg/1200x600wp.png",
+        link: "",
+    },
+    {
+        title: "The 48 Laws of Power",
+        description: `The 48 Laws of Power is a candid and controversial examination of power and its many dynamics. If you want to understand people and ascend in the world, this book is a good starting place.
+        You’ll learn about the nature of power, how to acquire it, and the dark ways in which people operate in the world. And in doing so, you’ll understand how to leverage power to get what you want, ideally using that knowledge to better the world.`,
+        category: "Personal Growth",
+        author: "",
+        image: "https://is1-ssl.mzstatic.com/image/thumb/Publication/v4/3d/d7/bb/3dd7bb73-c810-21b6-0e46-0cdfa478d8b0/9781101042458.jpg/1200x630wz.png",
+        link: "",
+    },
+    {
+        title: "Becoming Supernatural",
+        description: "Becoming Supernatural marries some of the most profound scientific information with ancient wisdom to show how people like you and me can experience a more mystical life. Readers will learn that we are, quite literally supernatural by nature if given the proper knowledge and instruction, and when we learn how to apply that information through various meditations, we should experience a greater expression of our creative abilities; that we have the capacity to tune in to frequencies beyond our material world and receive more orderly coherent streams of consciousness and energy; that we can intentionally change our brain chemistry to initiate profoundly mystical transcendental experiences; and how, if we do this enough times, we can develop the skill of creating a more efficient, balanced, healthy body, a more unlimited mind, and greater access to the realms of spiritual truth.",
+        category: "Personal Growth",
+        author: "Dr. Joe Dispenza",
+        image: "https://is4-ssl.mzstatic.com/image/thumb/Publication128/v4/4b/af/fe/4baffeae-d880-8202-3d36-6d67e23b123d/9781401953102.jpg/1200x630wz.png",
+        link: "",
     },
 ]
