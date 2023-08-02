01eb81672fb7d7b42ae7d84a7ce516116629df07
Dallas <71103219+LeleDallas@users.noreply.github.com>
2023-08-02 17:21:15 +0200

    feat: integration

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import Navbar from './navigator/Navbar'
+import About from './screens/About'
+import Aside from './navigator/Aside'
+import Resume from './screens/Resume'
+import { ActiveNav } from './types'
+import Contact from './screens/Contact'
+import Blog from './screens/Blog'
+import Portfolio from './screens/Portfolio'
+
+function App() {
+
+  const [active, setActive] = useState<ActiveNav>("About")
+
+  return (
+    <main>
+      <Aside />
+      <div className="main-content">
+        <Navbar active={active} setActive={setActive} />
+        <About active={active} />
+        <Resume active={active} />
+        <Contact active={active} />
+        <Blog active={active} />
+        <Portfolio active={active} />
+      </div>
+    </main>
+  )
+}
+
+export default App
